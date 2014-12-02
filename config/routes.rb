af7e2a379adf8bc5af9faa7f428cb5ef66d90dfb
Bryce Kerley <bkerley@brycekerley.net>
2014-12-01 19:38:21 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  resource :sessions
+  resource :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
