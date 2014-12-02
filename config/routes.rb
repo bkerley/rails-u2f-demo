d0d575de00790a1889ef442b89272b74e5ff24d4
Bryce Kerley <bkerley@brycekerley.net>
2014-12-01 20:18:00 -0500

    show stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'registrations/new'
+
   resource :sessions
   resource :users
+
+  root to: 'high_voltage/pages#show', id: 'dashboard'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
