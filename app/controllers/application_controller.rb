57f804589203bc0d1d6b0b000b45886d4b3885c1
Bryce Kerley <bkerley@brycekerley.net>
2014-12-01 20:18:22 -0500

    generate registrations controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def u2f
+    @u2f ||= U2F::U2F.new(request.base_url)
+  end
 end
